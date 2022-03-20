0919fe833e869da66a795d5fa67cdc4f2a0eb15a
ayah1996 <ayah.o.ezzeddine@gmail.com>
2022-03-20 16:40:53 +0200

    before replace google-map-react , static coordinates

--- src/App.js
+++ src/App.js
@@ -1,9 +1,37 @@
 import { CssBaseline, Grid } from "@material-ui/core";
+import { useEffect, useState } from "react";
+
+import { getPlacesData } from "./api/api";
+
 import Header from "./Components/Header/Header";
 import List from "./Components/List/List";
 import Map from "./Components/Map/Map";
 
 function App() {
+  const [places, setPlaces] = useState([]);
+  const [coordinates, setCoordinates] = useState({});
+  const [bounds, setBounds] = useState(null);
+
+  useEffect(() => {
+    // Run first time
+    navigator.geolocation.getCurrentPosition(
+      ({ coords: { latitude, longitude } }) => {
+        setCoordinates({ lat: latitude, lng: longitude });
+      }
+    );
+  }, []);
+
+  useEffect(() => {
+    // getPlacesData(bounds.sw, bounds.ne).then((data) => {
+    getPlacesData().then((data) => {
+      console.log(
+        "OUTPUT ~ file: App.js ~ line 17 ~ getPlacesData ~ data",
+        data
+      );
+
+      setPlaces(data);
+    });
+  }, [bounds, coordinates]);
   return (
     <>
       <CssBaseline />
@@ -13,7 +41,11 @@ function App() {
           <List />
         </Grid>
         <Grid item xs={12} md={8}>
-          <Map />
+          <Map
+            setCoordinates={setCoordinates}
+            setBounds={setBounds}
+            coordinates={coordinates}
+          />
         </Grid>
       </Grid>
     </>
